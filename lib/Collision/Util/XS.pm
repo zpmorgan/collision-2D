d434a6a5e322581fe9a2490cbe3f5cfc26a8de2e
Breno G. de Oliveira <garu@cpan.org>
2010-01-12 03:49:49 -0200

    placeholder for Collision::Util::XS

---
+++ lib/Collision/Util/XS.pm
@@ -0,0 +1,12 @@
+# TODO
+42;
+__END__
+
+=head1 Collision::Util::XS - A selection of general collision detection utilities (XS version)
+
+=head1 DESCRIPTION
+
+This is a placeholder for the yet-to-be Collision::Util::XS.
+
+Nothing to see here so far... please join #sdl in irc.perl.org if you care :)
+
