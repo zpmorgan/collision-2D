c632c26372b347098c7cdcb489ad96a944d392b2
zach morgan <zach@zach-netbook.(none)>
2010-03-22 23:39:16 -0400

    definitions for the other structs

--- inc/collision2d.h
+++ inc/collision2d.h
@@ -1,10 +1,33 @@
 
+#DEFINE ENT_STUFF \
+   float x, y; \
+   float xv, yv;\
+   float relative_x, relative_y;\
+   float relative_xv, relative_yv;\
 typedef struct Entity{
-   float x, y;
-   float xv, yv;
-   float relative_x, relative_y;
-   float relative_xv, relative_yv;
+   ENT_STUFF
 } Entity;
+
+typedef struct Point{
+   ENT_STUFF
+} Point;
+
+typedef struct Circle{
+   ENT_STUFF
+   float Radius;
+} Point;
+
+typedef struct Rect{
+   ENT_STUFF
+   float h,w;
+} Point;
+typedef struct Grid{
+   ENT_STUFF
+   AV* table;
+   float h,w;
+   int cells_x, cells_y;
+   float cell_size;
+} Point;
 
 typedef struct Collision{
    Entity* ent1;
